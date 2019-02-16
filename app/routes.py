9feb2a8e446b51da7d981fc741ee7cf9588c28b0
Eren Suner <gerensuner@gmail.com>
2019-02-16 13:13:21 -0500

    templates

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
