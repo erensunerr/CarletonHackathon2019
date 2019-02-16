55ecabb982552184150097b3400717348fa1fa4e
Eren Suner <gerensuner@gmail.com>
2019-02-16 15:37:13 -0500

    db config

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    date_of_registration = db.Column(db.Date(),index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
