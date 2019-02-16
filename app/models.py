122ebdaf79014f30bf15b3d2040c8fb2bffb5708
Eren Suner <gerensuner@gmail.com>
2019-02-16 18:05:17 -0500

    stuffz

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -9,3 +10,9 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
 from flask import render_template, request
 from app import app
+from werkzeug.security import generate_password_hash as hashme
+from werkzeug.security import check_password_hash as checkhash
+from flask_login import current_user, login_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -8,17 +12,41 @@ def index():
 
 @app.route('/login', methods=["POST","GET"])
 def login():
-    try:
-        username = request.form['username']
-        password = request.form['pass']
-        
-        return "WIHOO"
-    except:
-        pass
+    username, password = -1,-1
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    else:
+        try:
+            username = request.form['username']
+            password = request.form['pass']
+            remember_me = request.form['remember-me']
+        except:
+            pass
+        if username != -1 and password != -1:
+            user = User.query.filter_by(username=username).first()
+            if not(user is None) or user.check_password(password):
+                login_user()
+            else:
+                print('Invalid username or password')
     return render_template('login.html')
 
 @app.route('/sign_up', methods=['POST'])
 def sign_up():
+        username, password = -1,-1
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        else:
+            try:
+                username = request.form['username']
+                password = request.form['pass']
+
+            except:
+                pass
+            if username != -1 and password != -1:
+                user = User.query.filter_by(username=username).first()
+                if not :
+                    print('Invalid username or password')
+        return render_template('login.html')
     return render_template('sign_up.html')
 
 @app.route('/about')
