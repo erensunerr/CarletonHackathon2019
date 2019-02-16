eea2600304470e3caa49f779ab3727e9cc366452
SplittyFyre <oscar.xu.splitfire@gmail.com>
2019-02-16 13:26:34 -0500

    selenium kind of works

--- backend/webscraper.py
+++ backend/webscraper.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
+#chrome_driver_path = '/Users/oscarsplitfire/cuHacking2019/webdriver/chromedriver'
+chrome_driver_path = '../webdriver/chromedriver'
+
+driver = webdriver.Chrome(chrome_driver_path)
 
 driver.get("http://www.python.org")
 
