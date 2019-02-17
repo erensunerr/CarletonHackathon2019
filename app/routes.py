79c777b9604205e2bd42baf7fefa6f6f8a5fd995
SplittyFyre <oscar.xu.splitfire@gmail.com>
2019-02-17 03:36:44 -0500

    i hate

--- app/routes.py
+++ app/routes.py
@@ -14,9 +14,10 @@ def index():
 
 @app.route('/photo_handle', methods=["POST"])
 def photo_handle():
-    data = request.form["data"]
-    print('made past that request!!!!!!')
-    fout = open("img-" + datetime.utcnow(), "wb")
+    data = request.form.get("data", True)
+    #data = request.form['data']
+    print((data))
+    fout = open("img-" + str(datetime.utcnow()), "wb")
     data = base64.b64decode(data)
     fout.write(data)
     fout.close()
