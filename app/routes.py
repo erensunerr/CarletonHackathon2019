e1cf61cb9a68bbf31b2a7d6298c0c9de293b0a40
SplittyFyre <oscar.xu.splitfire@gmail.com>
2019-02-16 20:08:36 -0500

    fixed 1 internal error, and one Fatal_Eren_Exception

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,6 @@
 from flask import render_template, request, url_for, redirect
-<<<<<<< HEAD
 from app import app
-=======
 from app import app, db
->>>>>>> 81b359f4befcac1b4ebc20ef1ac60303f63d8da8
 from flask_login import current_user, login_user
 from app.models import User
 
@@ -12,16 +9,13 @@ from app.models import User
 def index():
     return render_template('index.html')
 
-<<<<<<< HEAD
 @app.route('/login', methods=["GET", "POST"])
-=======
 @app.route('/login', methods=["GET"])
 def display_login():
     return render_template('login.html')
 
 
-@app.route('/login_handle', methods=["POST","GET"])
->>>>>>> 81b359f4befcac1b4ebc20ef1ac60303f63d8da8
+@app.route('/login_handle', methods=["GET", "POST"])
 def login():
     username, password = -1, -1
     if current_user.is_authenticated:
@@ -35,16 +29,17 @@ def login():
             pass
         if username != -1 and password != -1:
             user = User.query.filter_by(username=username).first()
-            if not(user is None) or user.check_password(password):
+            if user and user.check_password(password):
                 login_user()
+                print('attempted to log in user')
             else:
-                render_template('login.html', error="Invalid password or username.")
+                return render_template('login.html', error="Invalid password or username.")
     return redirect(url_for('index'))
 
-@app.route('/sign_up', methods=['POST','GET'])
+@app.route('/sign_up', methods=['POST', 'GET'])
 def sign_up():
     global db
-    username, password, email, password2 = -1,-1,-1,-1
+    username, password, email, password2 = -1, -1, -1, -1
     if current_user.is_authenticated:
         return redirect(url_for('/index'))
     else:
@@ -55,7 +50,7 @@ def sign_up():
             email = request.form['email']
         except:
             pass
-        if username != -1 and password != -1 and email != -1 and password2 != -1 :
+        if not -1 in [username, email, password, password2]:
             user = User.query.filter_by(username=username).first()
             if not user:
                 u = User(username=username, email=email)
