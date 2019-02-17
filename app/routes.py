aad16c0cae90eda41e326828c38fb217878cbde5
Eren Suner <gerensuner@gmail.com>
2019-02-16 22:10:46 -0500

    asd

--- app/routes.py
+++ app/routes.py
@@ -64,7 +64,7 @@ def sign_up():
             return render_template('sign_up.html', error="Passwords do not match.")
 
         if not -1 in [username, email, password, password2]:
-            # check if user exists
+            # check if user exsits
             user = User.query.filter_by(username=username).first()
 
             if not user:    # if user does not exist, then create
@@ -79,7 +79,6 @@ def sign_up():
                 db.session.commit()
                 return redirect(url_for('index'))
 
-            else:   # else, say it already exists
                 return render_template('sign_up.html', error="Username already taken.")
         else:
             return render_template('sign_up.html', error="Please complete the form.")
